90f10215aa76e986cfbcd1091d0c21c3362dd83c
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2017-01-15 02:24:29 +0200

    autobuild

--- cibuild.sh
+++ cibuild.sh
@@ -6,6 +6,7 @@ echo "deb http://download.rethinkdb.com/apt `lsb_release -cs` main" | tee /etc/a
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
 echo "deb http://ftp.debian.org/debian unstable main contrib non-free" | tee /etc/apt/sources.list.d/unstable.list
 apt-get update
+apt-get -t unstable install -y libprotobuf9v5
 apt-get install -y rethinkdb
 rethinkdb --daemon
 
