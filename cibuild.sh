1a31b3a11dd685e6ad59c8cea421d64cd8ffe212
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2017-01-15 02:07:21 +0200

    autobuild

--- cibuild.sh
+++ cibuild.sh
@@ -1,10 +1,10 @@
 set -e
 
 
-echo "deb http://download.rethinkdb.com/apt `lsb_release -cs` main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
-wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install rethinkdb
+echo "deb http://download.rethinkdb.com/apt `lsb_release -cs` main" | tee /etc/apt/sources.list.d/rethinkdb.list
+wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
+apt-get update
+apt-get install rethinkdb
 rethinkdb --daemon
 
 nimble install -yd
