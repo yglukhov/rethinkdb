b112a1ee951e233ca43f50a1c88b46f913cd2dd8
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2017-01-15 02:10:46 +0200

    autobuild

--- cibuild.sh
+++ cibuild.sh
@@ -1,7 +1,7 @@
 set -e
 
 apt-get update
-apt-get install -y lsb-release
+apt-get install -y lsb-release wget gnupg
 echo "deb http://download.rethinkdb.com/apt `lsb_release -cs` main" | tee /etc/apt/sources.list.d/rethinkdb.list
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
 apt-get update
