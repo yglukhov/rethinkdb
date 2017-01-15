e70d1c9b13eae9e9c2d4f15975c60718515d1ba5
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2017-01-15 02:20:58 +0200

    autobuild

--- cibuild.sh
+++ cibuild.sh
@@ -1,10 +1,10 @@
 set -e
 
-echo "deb http://ftp.debian.org/debian unstable main contrib non-free" | tee /etc/apt/sources.list.d/unstable.list
 apt-get update
 apt-get install -y lsb-release wget gnupg
 echo "deb http://download.rethinkdb.com/apt `lsb_release -cs` main" | tee /etc/apt/sources.list.d/rethinkdb.list
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
+echo "deb http://ftp.debian.org/debian unstable main contrib non-free" | tee /etc/apt/sources.list.d/unstable.list
 apt-get update
 apt-get install -y rethinkdb
 rethinkdb --daemon
